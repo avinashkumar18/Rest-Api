e7e090fefff125250a85ad2ade72e3b0207dc83a
unknown <avinashkmrkr18@gmail.com>
2022-04-26 11:10:35 +0530

    code updates

--- src/main/java/com/server/controller/ServerController.java
+++ src/main/java/com/server/controller/ServerController.java
@@ -4,21 +4,26 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.server.dto.ServerDto;
 import com.server.repo.ServerRepo;
 
 @RestController
-public class ServerController {
+public class ServerController  {
 	
 	@Autowired
 	private ServerRepo repo;
+	
+
 	
 	@GetMapping("/hello")
 	public String name() {
@@ -32,18 +37,31 @@ public class ServerController {
 	}
 	
 	@GetMapping("/servers")
+	@ResponseBody
 	public List<ServerDto> getAllServers() {
-         return repo.findAll();
+			return repo.findAll();
      }
 	
 	@GetMapping("/servers/{id}")
-	public Optional<ServerDto> getServerById(@PathVariable String id){
-		return  repo.findById(id);
+	public Optional<ServerDto> getServerById(@PathVariable String id) {
+		try {
+			return  repo.findById(id);
+		}
+		catch(Exception e) {
+			throw new ResponseStatusException(
+			          HttpStatus.NOT_FOUND, "Server Not Found", e);
+		}
 	}
 	
 	@GetMapping("/server/{name}")
 	public List<ServerDto> getServerByName(@PathVariable String name){
-		return  repo.findByName(name);
+		try {
+			return  repo.findByName(name);
+		}
+		catch(Exception e) {
+			throw new ResponseStatusException(
+			          HttpStatus.NOT_FOUND, "Server Not Found", e);
+		}
 	}
 	
 	@DeleteMapping("/delete/{id}")
